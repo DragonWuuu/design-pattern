62c8a0c7acec9e17428a2cb23d662bed8de3fc21
DragonWuuu <3350986682@qq.com>
2025-08-12 20:48:41 +0800

    build(global):将根目录设置为父模块，新增工厂方法模式的子模块 删除根目录的可执行代码；新增工厂方法模式的相关代码；

--- factory-method-pattern/src/main/java/com/dragon/app/App.java
+++ factory-method-pattern/src/main/java/com/dragon/app/App.java
@@ -1,8 +1,14 @@
 package com.dragon.app;
 
+import lombok.extern.slf4j.Slf4j;
 
+/**
+ * Hello world!
+ */
+@Slf4j
 public class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        log.info("Hello World!");
     }
 }
